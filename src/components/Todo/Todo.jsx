df7c6074e66e21c6fdedb35a3f227dc3bb11f23c
gladja <gladkih.alexander@gmail.com>
2023-09-23 12:53:07 +0300

    add todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,17 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({counter, text, id, handleDelete}) => {
+
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{counter+1}
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton type="button" onClick={()=>handleDelete(id)}>
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,53 @@
-import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+import {Component} from 'react';
+import {nanoid} from 'nanoid';
+import {Grid, GridItem, SearchForm, EditForm, Text, Todo} from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  }
+  componentDidMount() {
+    const todos = JSON.parse(localStorage.getItem('todo'))
+    if (todos) this.setState({todos})
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    localStorage.setItem('todo', JSON.stringify(this.state.todos))
+  }
+
+  handleAddTodo = (text) => {
+    const todo = {
+      id: nanoid(),
+      text,
+    }
+    this.setState(prev => ({
+      todos: [...prev.todos, todo]
+    }))
+  }
+
+handleDelete = (id) => {
+    this.setState(prev => ({todos: prev.todos.filter(el => el.id !== id)}))
+}
+
   render() {
-    return <Text>Todos</Text>;
+    console.log(this.state.todos)
+    return (
+      <>
+        <SearchForm getSearchQuery={this.handleAddTodo} />
+
+        <Grid>
+          {this.state.todos.map(({id, text}, idx) => (
+            <GridItem key={id}>
+              <Todo
+                text={text}
+                counter={idx}
+                id={id}
+                handleDelete={this.handleDelete}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
